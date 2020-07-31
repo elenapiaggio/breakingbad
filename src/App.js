2567a5292d685d82c6667d3653f9bec968d28b89
Elena Piaggio <elenapiaggio@gmail.com>
2020-07-31 21:34:13 +0200

    IMPORT emotion in project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import styled from '@emotion/styled';
+import logo from './logo.png';
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 5rem;
+  flex-direction: column;
+`;
+
+const Button = styled.button`
+  background: -webkit-linear-gradient(top left, #007d35 0%, #007d35 40%, #0f574e 100%);
+  background-size: 300px;
+  font-family: Arial, Helvetica, sans-serif;
+  color: #fff;
+  margin-top: 3rem;
+  padding: 1rem 3rem;
+  font-size: 2rem;
+  border: 2px solid black;
+`;
 
 function App() {
   return (
-    <div className="App">
+    <Container>
       <h1>Get Breaking bad phrase</h1>
-    </div>
+      <Button>
+        Get phrase
+      </Button>
+    </Container>
   );
 }
 
